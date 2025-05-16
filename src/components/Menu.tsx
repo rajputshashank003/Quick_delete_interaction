c9b4c9fe23ee571d5e7222641d05609824e9f3b9
rajputshashank003 <hariomshashank@gmail.com>
2025-05-16 13:24:42 +0530

    feat: add undo feature fix: fix sm:width

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useState, type MouseEventHandler, type ReactNode } from "react"
+import { useRef, useState, type MouseEventHandler, type ReactNode } from "react"
 
 gsap.registerPlugin(useGSAP);
 
 const Menu = () => {
     const [ clicked , set_clicked ] = useState(false);
     const [ timer , set_timer ] = useState(6);
+    const delete_undo = useRef(false);
+    const timeout = useRef<number | null >(null);
+    const interval = useRef<number | null>(null);
 
     const handleClick = () => {    
 
@@ -57,12 +60,17 @@ const Menu = () => {
                 ease: "elastic.out(1.2, 0.3)",
             }, "abc");
 
-            const interval = setInterval(() => {
+            interval.current = setInterval(() => {
                 set_timer(prev => prev - 1);
             }, 1000);
     
-            setTimeout(() => {
-                clearInterval(interval);
+            timeout.current = setTimeout(() => {
+                if ( interval.current ) {
+                    clearInterval(interval.current);
+                }
+                if (delete_undo.current) {
+                    return;
+                }
                 gsap.to('.the_par', {
                     opacity: 0,
                     duration: 0.4
@@ -92,18 +100,98 @@ const Menu = () => {
 
 
     };
+
+    const handleUndo = () => {
+        delete_undo.current = true;
+        if ( timeout.current ){
+            clearTimeout(timeout.current)
+        }
+        if ( interval.current ){
+            clearInterval(interval.current)
+        }
+        set_clicked(false);
+        set_timer(6)
+        const tl = gsap.timeline();
+        tl.to(".name", {
+            opacity: 1,
+            duration : 0
+        }, "d")
+        .to('.options', {
+            opacity: 1,
+            duration : 0
+        }, "d")
+        .to('.hr_line', {
+            opacity: 1,
+            duration : 0
+        }, "d")
+
+        .to(".name", {
+            display: 'flex',
+        }, "abc")
+        .to('.options', {
+            display: 'flex',
+        }, "abc")
+        .to('.hr_line', {
+            display: 'flex',
+        }, "abc")
+        .to('.the_par', {
+            height: 410,
+            duration : 0.3,
+            ease: "elastic.out(2, 5)",
+        }, "abc")
+        .to('.deleted', {
+            position: 'absolute',
+        },'abc')
+        .to('.left', {
+            y : -80,
+            opacity: 0,
+            delay: 0.1,
+            duration: 0,
+            ease: "elastic.out(1.2, 0.3)",
+        }, "abc")
+        .to('.right', {
+            opacity: 0,
+            delay: 0.1,
+            duration: 0,
+            ease: "elastic.out(1.2, 0.3)",
+        }, "abc")
+        .to('.right', {
+            y : 80,
+            duration: 0,
+            ease: "elastic.out(1.2, 0.3)",
+        });
+
+        const tl2 = gsap.timeline();
+        tl2.to(".value1", {
+            x: 0,
+            opacity: 1,
+            duration: 0.3,
+            ease: "power2.out",
+            position: 'relative'
+        }, "abc")
+        .to( '.value2', {
+            x: 200,
+            opacity: 0,
+            duration: 0.8,
+            ease: "elastic.out(1.2, 0.3)",
+          },
+          "abc",
+        );
+
+    }
+
     return (
-        <div className="h-fit the_par relative comic-relief-regular max-sm:w-[350px] w-[400px] text-zinc-200 rounded-3xl shadow-xl shadow-black/50 bg-zinc-800 ">
-            <div className="deleted max-sm:px-3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[30px] p-5 z-[99] absolute grid grid-cols-6 ">
-                <div className="left max-sm:col-span-3 -translate-y-[80px] text-red-400 flex flex-row items-center gap-3 opacity-0  col-span-4">
+        <div className="h-[410px] the_par relative comic-relief-regular max-sm:max-w-[340px] w-[355px] text-zinc-200 rounded-3xl shadow-xl shadow-black/50 bg-zinc-800 ">
+            <div className="deleted max-sm:px-3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[30px] p-5 z-[99] absolute flex flex-row justify-between items-center">
+                <div className="left max-sm:col-span-3 -translate-y-[80px] text-red-400 flex flex-row items-center gap-2 opacity-0 col-span-4">
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="38"  height="38"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-square-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" /><path d="M9 12l2 2l4 -4" /></svg>
                     Deleted
                 </div>
-                <div className="right flex max-sm:col-span-3 max-sm:justify-end flex-row items-center gap-3 translate-y-[80px] opacity-0 col-span-2">
+                <div className="right flex max-sm:col-span-3 max-sm:justify-end flex-row items-center gap-2 translate-y-[80px] opacity-0 col-span-2">
                     <div className="h-[40px] rounded-full w-[40px] text-[25px] text-zinc-800 flex justify-center items-center bg-gray-300/80">
                         {timer}
                     </div>
-                    <div className="cursor-pointer hover:bg-zinc-700 px-1 rounded-xl duration-200">
+                    <div onClick={handleUndo} className="cursor-pointer hover:bg-zinc-700 px-1 rounded-xl duration-200">
                         Undo
                     </div>
                 </div>
