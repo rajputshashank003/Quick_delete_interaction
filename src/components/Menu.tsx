2027f41d1d02664be6be20566c74c5d562b0e553
rajputshashank003 <hariomshashank@gmail.com>
2025-05-16 10:27:21 +0530

    fix: fix timeout bug and  add more animation

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -55,7 +55,20 @@ const Menu = () => {
                 delay: 0.1,
                 duration: 0.8,
                 ease: "elastic.out(1.2, 0.3)",
-            }, "abc")
+            }, "abc");
+
+            const interval = setInterval(() => {
+                set_timer(prev => prev - 1);
+            }, 1000);
+    
+            setTimeout(() => {
+                clearInterval(interval);
+                gsap.to('.the_par', {
+                    opacity: 0,
+                    duration: 0.4
+                })
+            }, 6000);
+
             return ;
         }
         set_clicked(true);
@@ -77,17 +90,6 @@ const Menu = () => {
           "abc",
         );
 
-        const interval = setInterval(() => {
-            set_timer(prev => prev - 1);
-        }, 1000);
-
-        setTimeout(() => {
-            clearInterval(interval);
-            gsap.to('.the_par', {
-                opacity: 0,
-                duration: 0.4
-            })
-        }, 6000);
 
     };
     return (
@@ -110,21 +112,21 @@ const Menu = () => {
                 Merchant Options
             </div>
             <hr className="w-full hr_line" />
-            <div className="options flex p-4 px-3 flex-col gap-3 text-[32px] ">
+            <div className="options w-full overflow-hidden flex p-4 px-3 flex-col gap-3 relative z-[99] text-[32px] ">
                 <Option>
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="36"  height="36"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" /><path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" /><path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" /><path d="M8.56 20.31a9 9 0 0 0 3.44 .69" /><path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" /><path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" /><path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" /><path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="currentWidth"  height="currentHeight"    viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon group-hover:scale-120 h-[36px] w-[36px] group-hover:h-[42px] group-hover:-translate-y-[18px] duration-300 group-hover:rotate-[-20deg] icon-tabler icons-tabler-outline icon-tabler-circle-dashed-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" /><path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" /><path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" /><path d="M8.56 20.31a9 9 0 0 0 3.44 .69" /><path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" /><path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" /><path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" /><path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
                     Add New
                 </Option>
                 <Option>
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="38"  height="38"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="currentWidth"  height="currentHeight"    viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon group-hover:scale-120 h-[38px] w-[38px] group-hover:h-[42px] group-hover:-translate-y-[18px] duration-300 group-hover:rotate-[-20deg] icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                     Manage
                 </Option>
                 <Option>
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="38"  height="38"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-archive"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" /><path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" /><path d="M10 12l4 0" /></svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="currentWidth"  height="currentHeight"    viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon group-hover:scale-120 h-[38px] w-[38px] group-hover:h-[42px] group-hover:-translate-y-[18px] duration-300 group-hover:rotate-[-20deg] icon-tabler icons-tabler-outline icon-tabler-archive"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" /><path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" /><path d="M10 12l4 0" /></svg>
                     Archive
                 </Option>
                 <Option onClick={handleClick} className='text-red-400' >
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="38"  height="38"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon z-[99] icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="currentWidth"  height="currentHeight"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon z-[99] icon-tabler group-hover:scale-140 h-[38px] w-[38px] group-hover:h-[42px] group-hover:-translate-y-[18px] duration-300 group-hover:rotate-[-20deg] icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                     <div className="value1">
                         Delete
                     </div>
@@ -139,7 +141,7 @@ const Menu = () => {
 
 const Option = (props: { children: ReactNode, onClick?: MouseEventHandler, className?: string }) => {
     return (
-        <div onClick={props.onClick} className={` ${props.className} flex relative overflow-hidden h-fit w-full px-4 py-2 cursor-pointer duration-200 rounded-[26px] hover:bg-zinc-600 flex-row gap-2 justify- items-center`}>
+        <div onClick={props.onClick} className={` ${props.className} group flex relative h-fit w-full px-4 py-2 cursor-pointer duration-300 rounded-[26px] hover:bg-zinc-600 flex-row gap-2 justify- items-center`}>
             {props.children}
         </div>
     )
